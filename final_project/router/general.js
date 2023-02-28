78535fe05f561414bc769f228fcbecccf08d12ef
ArthCarvalho <carvalhosouzalima.arthur@gmail.com>
2023-02-28 00:44:19 -0500

    Added comment back

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,6 +35,7 @@ public_users.post("/register", (req,res) => {
     });
 });
 
+// Get the book list available in the shop
 public_users.get('/', async (req, res) => 
     new Promise((resolve, reject) => {
         resolve(books);
